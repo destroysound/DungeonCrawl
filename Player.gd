a1b8b5bd50be1d0064bb7687493b9dabcb13b81f
Kylie <tinkerchelle93@yahoo.com>
2020-09-04 22:07:47 -0400

    added player facing

--- Player.gd
+++ Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
-var speed : = 400.0
+var speed : = 150.0
 var path : = PoolVector2Array() setget set_path
+
+onready var animator : AnimatedSprite = get_node("AnimatedSprite")
 
 
 func _ready() -> void:
@@ -36,3 +38,37 @@ func set_path(value : PoolVector2Array) -> void:
 	path = value
 	path.remove(0)
 	set_process(true)
+	
+var _position_last_frame := Vector2()
+var _cardinal_direction = 0
+
+func _physics_process(delta):
+	# Get motion vector between previous and current position
+	var motion = position - _position_last_frame
+
+	# If the node actually moved, we'll recompute its direction.
+	# If it didn't, we'll just the last known one.
+	if motion.length() > 0.0001:
+		# Now if you want a value between N.S.W.E,
+		# you can use the angle of the motion.
+		# I came up with this formula last time I needed it:
+		_cardinal_direction = int(4.0 * (motion.rotated(PI / 4.0).angle() + PI) / TAU)
+
+	# And now you have it!
+	# You can even use it with an array since it's like an index
+	match _cardinal_direction:
+		0:
+			animator.play("walk_left")
+			print("West")
+		1:
+			animator.play("walk_up")
+			print("North")
+		2:
+			animator.play("walk_right")
+			print("East")
+		3:
+			animator.play("walk_down")
+			print("South")
+
+	# Remember our current position for next frame
+	_position_last_frame = position
