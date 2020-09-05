56e65e9d100af8cd29a2823982e74aca4c2d4369
Willow Hunt <whunt@mosaiclearning.com>
2020-09-04 20:31:50 -0400

    Navigation pt1

---
+++ GameScene.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+onready var nav_2d : Navigation2D = $Navigation2D
+onready var character : KinematicBody2D = $Player
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if not event is InputEventMouseButton:
+		return
+	if event.button_index != BUTTON_LEFT or not event.pressed:
+		return
+
+	var new_path : = nav_2d.get_simple_path(character.global_position, event.global_position)
+	character.path = new_path
